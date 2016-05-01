5743736bca2e86aeb2b3def7fd3f109330a31832
Danilo Bustos <danilo.bustos@equifax.com>
2016-04-30 21:39:02 -0700

    gitignore changes and removed .idea folder

--- src/main/java/tweetsTokenizer.java
+++ src/main/java/tweetsTokenizer.java
@@ -1,5 +1,35 @@
+import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 /**
  * Created by Danilo Bustos on 4/30/16.
  */
 public class tweetsTokenizer {
+    String emoticon_string = "(?:[<>]?[:;=8][\\-o\\*\\']?[\\)\\]\\(\\[dDpP/\\:\\}\\{@\\|\\\\]|[\\)\\]\\(\\[dDpP/\\:\\}\\{@\\|\\\\][\\-o\\*\\']?[:;=8][<>]?)";
+
+    Pattern html_entity_digit = Pattern.compile("&#\\d+;");
+
+    String html_entity_alpha = "&\\w+";
+
+    public List<String> tokenize(String _tweet) {
+        String tweet = _tweet;
+
+        Matcher m = html_entity_digit.matcher(tweet);
+
+        while (m.find()) {
+            m.group(0);
+        }
+
+
+
+    }
+
+    private String html2unicode(String tweet) {
+
+    }
+
+
+
+
 }
